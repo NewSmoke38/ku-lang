691247fec41d2a498479ac58c18bcafb6032f76c
Shivani <shivanialive2007@gmail.com>
2025-07-14 01:59:29 +0530

    add links to the navigation bar

--- bin/ku.js
+++ bin/ku.js
@@ -3,7 +3,7 @@
 import { fileURLToPath } from 'url';
 import { dirname, resolve } from 'path';
 import fs from 'fs';
-import { runKu } from '../interpreter/ku.js';
+import { runKu } from '../ku.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
